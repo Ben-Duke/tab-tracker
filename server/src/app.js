da363683d5ed1630170d45925d1c1109bf47c43b
Benjamin Duke <bad38@uclive.ac.nz>
2019-01-07 00:20:37 +1300

    Setting up the registration page

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+console.log('Hello')
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyparser.json())
+app.use(cors())
+
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email} your user was registered! have fun!`
+    })
+})
+
+
+app.listen(process.env.PORT || 8081)
